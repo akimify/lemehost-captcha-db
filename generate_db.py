5155a42dda987f164bcf40d550fe75f75de4addc
akimify <ak1ra0chan9@gmail.com>
2026-03-04 00:10:28 +0800

    Update generate_db.py

--- generate_db.py
+++ generate_db.py
@@ -1,16 +1,30 @@
-import json, random, string, urllib.request
+import json
+import random
+import string
+import urllib.request
 from PIL import Image, ImageDraw, ImageFont
 
-FONT_PATH    = 'SpicyRice.ttf'
-IMG_W        = 120
-IMG_H        = 50
-FONT_SIZE    = 30
-OFFSET       = -2
-COUNT        = 500
-CODE_LEN     = 6
-OUTPUT       = 'db.json'
+FONT_PATH  = 'SpicyRice.ttf'
+IMG_W      = 120
+IMG_H      = 50
+FONT_SIZE  = 30
+OFFSET     = -2
+COUNT      = 500
+CODE_LEN   = 6
+OUTPUT     = 'db.json'
 
 CHARS = string.ascii_lowercase
+
+def download_font():
+    try:
+        ImageFont.truetype(FONT_PATH, FONT_SIZE)
+    except:
+        print('下载 SpicyRice.ttf ...')
+        urllib.request.urlretrieve(
+            'https://github.com/yiisoft/yii/raw/master/framework/web/widgets/captcha/SpicyRice.ttf',
+            FONT_PATH
+        )
+        print('下载完成')
 
 def render_captcha(code):
     img  = Image.new('RGB', (IMG_W, IMG_H), (255, 255, 255))
@@ -21,7 +35,7 @@ def render_captcha(code):
         angle = random.uniform(-15, 15)
         tmp   = Image.new('RGBA', (40, 50), (255, 255, 255, 0))
         ImageDraw.Draw(tmp).text((0, 0), ch, font=font, fill=(0, 0, 0, 255))
-        tmp = tmp.rotate(angle, expand=False, fillcolor=(255, 255, 255, 0))
+        tmp   = tmp.rotate(angle, expand=False, fillcolor=(255, 255, 255, 0))
         img.paste(tmp, (x, y - FONT_SIZE), mask=tmp.split()[3])
         bbox  = font.getbbox(ch)
         x    += bbox[2] - bbox[0] + OFFSET
@@ -37,16 +51,7 @@ def img_to_pixels(img):
     return pixels
 
 def main():
-    try:
-        ImageFont.truetype(FONT_PATH, FONT_SIZE)
-    except:
-        print('下载 SpicyRice.ttf ...')
-        urllib.request.urlretrieve(
-            'https://github.com/yiisoft/yii/raw/master/framework/web/widgets/captcha/SpicyRice.ttf',
-            FONT_PATH
-        )
-        print('下载完成')
-
+    download_font()
     db = []
     for i in range(COUNT):
         code   = ''.join(random.choices(CHARS, k=CODE_LEN))
